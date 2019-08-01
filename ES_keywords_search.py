2119bc1e21ac4ce2aca7f91977da4bc8cf8f95cf
wuyaqiang <wuyaqiang0202@outlook.com>
2019-08-01 17:37:12 +0800

    2019-8-1

--- ES_keywords_search.py
+++ ES_keywords_search.py
@@ -2,6 +2,7 @@ from tqdm import tqdm
 import os
 import re
 import time
+import json
 import codecs
 import jieba
 import pickle
@@ -11,72 +12,99 @@ from elasticsearch.client import IndicesClient
 import pymysql
 import mysql.connector as sql
 
-time_1 = time.time()
+def fetch_mysql_data(db_conn, table_name, table_fields):
+    '''
+    :param db_conn:
+    :param table_name:
+    :param table_fields:
+    :return:
+    '''
+    cur = db_conn.cursor()
+    sql_command = " SELECT " + ",".join(table_fields) + " FROM " + table_name
 
-es = Elasticsearch([{'host':'localhost','port':9200}])
+    cur.execute(sql_command)
+    fetch_results = list(cur.fetchall())
 
-es_idx = IndicesClient(es)
+    return fetch_results
 
-es_idx.delete(index='_all')
+def json_data_gen(all_data, data_table_name, data_fields, data_start_id):
+    '''
+    :param all_data:
+    :param data_table_name:
+    :param data_fields:
+    :return:
+    '''
+    with open('./json_data_temp.json', 'w', encoding='utf-8') as f:
+        for idx, item in enumerate(all_data):
+            index_line = json.dumps({"index": {"_index": data_table_name, "_type": "docs", "_id": str(data_start_id + idx + 1)}}) + '\n'
+            data_dict = {}
+            for i, field in enumerate(data_fields):
+                data_dict[field] = item[i]
+            data_line = json.dumps(data_dict) + '\n'
+            f.writelines([index_line, data_line])
 
-a = os.popen('''
-curl -X PUT 'localhost:9200/pubmed_articles' -d '
-{
-  "mappings": {
-    "docs": {
-      "properties": {
-        "pmid": {
-          "type": "text"
-        },
-        "title": {
-          "type": "text",
-          "analyzer": "ik_smart",
-          "search_analyzer": "ik_smart"
-        }
-      }
-    }
-  }
-}'
-''').read()
-for line in a.split('\n'):
-    print(line)
+def build_index(post_batch, table_name, table_fields):
+    '''
+    :param post_batch:
+    :param table_name:
+    :param table_fields:
+    :return:
+    '''
+    start_time = time.time()
+    properties_dict = {}
+    for field in table_fields:
+        properties_dict[field] = {"type": "text",
+                                  "analyzer": "ik_max_word",
+                                  "search_analyzer": "ik_max_word"}
+    index_body_dict = { "mappings": { "docs": { "properties": properties_dict } } }
+    create_index_command = "curl -X PUT 'localhost:9200/" + table_name + "' -d '" + json.dumps(index_body_dict) + "'"
+    os.system(create_index_command)
 
-# fetch all data from mysql:
-conn = pymysql.connect(host = 'rm-2zet5lw17as40fty28o.mysql.rds.aliyuncs.com',
-                       port = 3306,
-                       user = 'snowball',
-                       passwd = 'MEDOsnow$%^&',
-                       db = 'medo_master')
-cur = conn.cursor()
-sql_command = "SELECT pmid, title FROM pubmed_articles"
+    conn = pymysql.connect(host='rm-2zet5lw17as40fty28o.mysql.rds.aliyuncs.com',
+                           port=3306,
+                           user='snowball',
+                           passwd='MEDOsnow$%^&',
+                           db='medo_master')
 
-cur.execute(sql_command)
-res = cur.fetchall()
-print(len(res))
+    res = fetch_mysql_data(conn, table_name, table_fields)
 
-doc_list = []
-for item in res:
-    doc = {"pmid": item[0], "title": item[1]}
-    doc_list.append(doc)
-print(len(doc_list))
-print(doc_list[0])
+    for idx in range(0, len(res), post_batch):
+        json_data_gen(res[idx : idx + post_batch], table_name, table_fields, idx)
+        batch_post_command = "curl -XPOST localhost:9200/_bulk --data-binary @json_data_temp.json"
+        os.system(batch_post_command)
 
-for doc in doc_list:
-    temp = os.popen('''
-    curl -X POST 'localhost:9200/pubmed_articles/docs' -d '
-    {}
-    '
-    '''.format(str(doc).replace("\"", "\'").replace("\'", "\""))).read()
-    for line in temp.split('\n'):
-        print(line)
+    return (table_name, table_fields, len(res), time.time() - start_time)
 
 
-print('all time: ', time.time() - time_1)
+def query_keyword(index_name, query_field, query_keyword):
+    '''
+    :param index_name:
+    :param query_field:
+    :param query_keyword:
+    :return:
+    '''
+    es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+    search_body_dict = { "query": {"term": {query_field : query_keyword} } }
+    results = es.search(index = index_name, body = json.dumps(search_body_dict) )
+    hits = results['hits']['hits']
+    for idx, res in enumerate(hits):
+        print(str(idx + 1) + ":  " + res['_source']['comSummary'])
 
-# tokens_res = es_idx.analyze(index='pubmed_articles', body={
-# #     "analyzer": "ik_max_word",
-#     "analyzer": "ik_smart",
-#     "text" : "HiFreSP: A novel high-frequency sub-pathway mining approach to identify robust prognostic gene signatures."
-# })
-# for token in tokens_res['tokens']:
-#     print(token['token'])
+
+if __name__ == '__main__':
+
+    # table_name, table_fields, data_num, spend_time = build_index(20000, "nsfc_data", ["applyCode", "comSummary"])
+    # print("Build Index Done.")
+    # print("Index Table: ", table_name)
+    # print("Index Table Fields: ", table_fields)
+    # print("Number of Data: ", data_num)
+    # print("Building Time: ", spend_time)
+
+    query_keyword("nsfc_data", "comSummary", "心脏毒性")
+
+
+
+
+
+
+
